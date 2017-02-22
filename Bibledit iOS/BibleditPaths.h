d82426380656b57b507cf5b79e342a364ce678e1
Teus Benschop <teusjannette@gmail.com>
2017-02-22 19:52:05 +0100

    Move iOS port to separate repo
    https://github.com/bibledit/bibledit/issues/340

---
+++ Bibledit iOS/BibleditPaths.h
@@ -0,0 +1,16 @@
+//
+//  BibleditPaths.h
+//  Bibledit
+//
+//  Created by Mini on 13-09-14.
+//  Copyright (c) 2014 Teus Benshop. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BibleditPaths : NSObject
+
++ (NSString *)resources;
++ (NSString *)documents;
+
+@end
