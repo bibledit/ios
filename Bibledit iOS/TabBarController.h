2d495a491518396bfc488bc053425310f78d6653
Teus Benschop <teusjannette@gmail.com>
2017-05-11 14:44:15 +0200

    Add proper copyright notices to iOS code
    This fixes https://github.com/bibledit/bibledit/issues/557

--- Bibledit iOS/TabBarController.h
+++ Bibledit iOS/TabBarController.h
@@ -1,10 +1,21 @@
-//
-//  TabBarController.h
-//  Bibledit
-//
-//  Created by Teus Benschop on 07/05/2017.
-//  Copyright © 2017 Teus Benschop. All rights reserved.
-//
+/*
+ Copyright (©) 2003-2017 Teus Benschop.
+ 
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 
 #import <UIKit/UIKit.h>
 
