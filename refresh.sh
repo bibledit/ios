cbaa63552cc0c279371a3130f1a5d02803f6d760
Teus Benschop <teusjannette@gmail.com>
2025-08-10 13:55:47 +0200

    Simplify error handling in refresh.sh
    Removed checks for command exit status and added 'set -e' to exit on errors automatically.
    https://github.com/bibledit/cloud/issues/1026

--- refresh.sh
+++ refresh.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 
+# Exit script on error.
+set -e
+
+
 echo Refresh / clean / build the Bibledit kernel source for iOS.
 
 
 IOSSOURCE=`dirname $0`
-if [ $? -ne 0 ]; then exit; fi
 cd $IOSSOURCE
-if [ $? -ne 0 ]; then exit; fi
 IOSSOURCE=`pwd`
-if [ $? -ne 0 ]; then exit; fi
 echo The source path is $IOSSOURCE
 
 
@@ -20,9 +21,7 @@ WEBROOT=Bibledit/webroot
 
 echo Copying Bibledit kernel files from $KERNELSOURCE to $KERNELDEST
 mkdir -p $KERNELDEST
-if [ $? -ne 0 ]; then exit; fi
 rsync --archive --delete --exclude bridging_header.h --exclude cpp_file.* $KERNELSOURCE/ $KERNELDEST/
-if [ $? -ne 0 ]; then exit; fi
 
 
 # List distinct file suffixes:
@@ -31,48 +30,30 @@ if [ $? -ne 0 ]; then exit; fi
 
 echo Prepare and clean the Bibledit kernel code
 pushd $KERNELDEST
-if [ $? -ne 0 ]; then exit; fi
 rm -rf .git
-if [ $? -ne 0 ]; then exit; fi
 echo Configure Bibledit in client mode
 echo Run only one parallel task so the interface is more responsive
 echo Enable the single-tab browser
 ./configure --enable-ios
-if [ $? -ne 0 ]; then exit; fi
 make clean
-if [ $? -ne 0 ]; then exit; fi
 echo Switch to MbedTLS 2.x
 rm -rf mbedtls
-if [ $? -ne 0 ]; then exit; fi
 mv mbedtls2 mbedtls
-if [ $? -ne 0 ]; then exit; fi
 echo Remove all files except C and C++ source code
 find . -type f ! -name '*.h' ! -name '*.hpp' ! -name '*.c' ! -name '*.cpp' ! -name '*.cxx' -delete
 rm -f .DS_Store
-if [ $? -ne 0 ]; then exit; fi
 rm -rf .github
-if [ $? -ne 0 ]; then exit; fi
 rm -rf autom4te.cache
-if [ $? -ne 0 ]; then exit; fi
 rm -rf xcode.xcodeproj
-if [ $? -ne 0 ]; then exit; fi
 rm -rf unittests
-if [ $? -ne 0 ]; then exit; fi
 rm -rf sources
-if [ $? -ne 0 ]; then exit; fi
 rm -rf executable
-if [ $? -ne 0 ]; then exit; fi
 find . -name .deps -type d -delete
-if [ $? -ne 0 ]; then exit; fi
 find . -name exampleProgram.cpp -delete
-if [ $? -ne 0 ]; then exit; fi
 find . -name shell.c -delete
-if [ $? -ne 0 ]; then exit; fi
 rm -rf i18n
-if [ $? -ne 0 ]; then exit; fi
 echo Update the configuration: No external SWORD / ICU / UTF8PROC / PUGIXML libraries
 sed -i.bak '/HAVE_SWORD/d' config.h
-if [ $? -ne 0 ]; then exit; fi
 sed -i.bak '/HAVE_ICU/d' config.h
 if [ $? != 0 ]; then exit; fi
 sed -i.bak '/HAVE_UTF8PROC/d' config.h
@@ -81,90 +62,55 @@ sed -i.bak '/HAVE_PUGIXML/d' config.h
 if [ $? != 0 ]; then exit; fi
 echo The embedded web view cannot upload files
 sed -i.bak '/CONFIG_ENABLE_FILE_UPLOAD/d' config/config.h
-if [ $? -ne 0 ]; then exit; fi
 find . -name '*.bak' -delete
-if [ $? -ne 0 ]; then exit; fi
 popd
-if [ $? -ne 0 ]; then exit; fi
 
 
 echo Copying Bibledit kernel files from $KERNELSOURCE to $WEBROOT
 mkdir -p $WEBROOT
-if [ $? -ne 0 ]; then exit; fi
 rsync --archive --delete $KERNELSOURCE/  $WEBROOT/
-if [ $? -ne 0 ]; then exit; fi
 
 
 echo Prepare and clean the Bibledit kernel webroot
 pushd $WEBROOT
-if [ $? -ne 0 ]; then exit; fi
 echo Build several databases and other data for inclusion with the iOS package
 echo Building them on iOS would take a lot of time during the setup phase
 echo Including pre-built data speeds up the setup phase of Bibledit on iOS
 ./configure
-if [ $? -ne 0 ]; then exit; fi
 make --jobs=`sysctl -n hw.ncpu`
-if [ $? -ne 0 ]; then exit; fi
 ./generate . locale
-if [ $? -ne 0 ]; then exit; fi
 ./generate . mappings
-if [ $? -ne 0 ]; then exit; fi
 ./generate . versifications
-if [ $? -ne 0 ]; then exit; fi
 echo Remove the journal entries that were logged in the process
 rm -f logbook/1*
-if [ $? -ne 0 ]; then exit; fi
 make distclean
-if [ $? -ne 0 ]; then exit; fi
 rm -rf .git*
-if [ $? -ne 0 ]; then exit; fi
 echo Remove the C and C++ source code
 find . -name '*.h' -delete
-if [ $? -ne 0 ]; then exit; fi
 find . -name '*.hpp' -delete
-if [ $? -ne 0 ]; then exit; fi
 find . -name '*.c' -delete
-if [ $? -ne 0 ]; then exit; fi
 find . -name '*.cpp' -delete
-if [ $? -ne 0 ]; then exit; fi
 find . -name '*.cxx' -delete
-if [ $? -ne 0 ]; then exit; fi
 rm -f .DS_Store
-if [ $? -ne 0 ]; then exit; fi
 rm -rf autom4te.cache
-if [ $? -ne 0 ]; then exit; fi
 rm -rf xcode.xcodeproj
-if [ $? -ne 0 ]; then exit; fi
 rm -rf unittests
-if [ $? -ne 0 ]; then exit; fi
 rm -rf sources
-if [ $? -ne 0 ]; then exit; fi
 rm -rf executable
-if [ $? -ne 0 ]; then exit; fi
 find . -name .deps -type d -delete
-if [ $? -ne 0 ]; then exit; fi
 rm -rf i18n
-if [ $? -ne 0 ]; then exit; fi
 find . -maxdepth 1 -type f -delete
-if [ $? -ne 0 ]; then exit; fi
 find . -name ".dirstamp" -delete
-if [ $? -ne 0 ]; then exit; fi
 rm locale/README
-if [ $? -ne 0 ]; then exit; fi
 find . -name '*.sh' -delete
-if [ $? -ne 0 ]; then exit; fi
 rm -rf mbedtls*
-if [ $? -ne 0 ]; then exit; fi
 find . -name .deps -ls -exec rm -rv {} +
-if [ $? -ne 0 ]; then exit; fi
 popd
-if [ $? -ne 0 ]; then exit; fi
 
 
 echo Convert the file hierarchy in the webroot to a flat structure
 
 pushd $WEBROOT
-if [ $? -ne 0 ]; then exit; fi
 
 echo Creating array of directories in webroot
 directories=($(find . -type d))
@@ -202,8 +148,7 @@ do
   # Move the original file to a new file like 'file#help#changelog.html.res'.
   file2=file#${file2}.res
   mv $file $file2
-  if [ $? -ne 0 ]; then exit; fi
-done
+  done
 
 echo Remove the now empty original directories
 for directory in ${directories[@]}
@@ -212,6 +157,5 @@ do
 done
 
 popd
-if [ $? -ne 0 ]; then exit; fi
 
 echo Succesfully refreshed the Bibledit kernel
