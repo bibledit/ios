89628f2f60932b3eda4fb78840e0e761b3450ff6
Teus Benschop <teusjannette@gmail.com>
2025-08-13 17:30:23 +0200

    Switch to MbedTLS version 3.x
    ... and so remove version 2.x.
    https://github.com/bibledit/cloud/issues/973

--- refresh.sh
+++ refresh.sh
@@ -36,9 +36,8 @@ echo Run only one parallel task so the interface is more responsive
 echo Enable the single-tab browser
 ./configure --enable-ios
 make clean
-echo Switch to MbedTLS 2.x
-rm -rf mbedtls
-mv mbedtls2 mbedtls
+echo Use MbedTLS 3.x so remove version 2.x
+rm -rf mbedtls2
 echo Remove all files except C and C++ source code
 find . -type f ! -name '*.h' ! -name '*.hpp' ! -name '*.c' ! -name '*.cpp' ! -name '*.cxx' -delete
 rm -f .DS_Store
